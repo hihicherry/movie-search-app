78d50c937e0bb0a78c665d428fea34108275bc99
Cherry Chung <bubibuuu@gmail.com>
2025-03-26 11:55:12 +0800

    優化讀取localstorage

--- src/contexts/MovieContext.jsx
+++ src/contexts/MovieContext.jsx
@@ -5,48 +5,46 @@ const MovieContext = createContext();
 export const useMovieContext = () => useContext(MovieContext);
 
 export const MovieProvider = ({children}) => {
-    //初始化時讀取localstorage的資料
-    const [favorites, setFavorites] = useState([]);
+	const [favorites, setFavorites] = useState(() => {
+		//初始化時讀取localstorage的資料
+		const storedFavs = localStorage.getItem("favorites");
+		return storedFavs ? JSON.parse(storedFavs) : [];
+	});
 
-    useEffect(() => {
-        const storedFavs = localStorage.getItem("favorites");
+	// 監聽favorites變化，存入localStorage
+	useEffect(() => {
+		localStorage.setItem("favorites", JSON.stringify(favorites));
+	}, [favorites]);
 
-        if(storedFavs) setFavorites(JSON.parse(storedFavs));
-    },[])
-
-    useEffect(() => {
-        localStorage.setItem("favorites", JSON.stringify(favorites));
-    }, [favorites]);
-
-    const addToFavorites = (item, mediaType) => {
-		setFavorites((prev) => [
-			...prev, {...item, mediaType},
-		]);
+	const addToFavorites = (item, mediaType) => {
+		setFavorites((prev) => [...prev, { ...item, mediaType }]);
 	};
 
-    const removeFromFavorites = (itemId, mediaType) => {
-        setFavorites((prev) => prev.filter(
-            (item) => item.id !== itemId || item.mediaType !== mediaType
-        ));
-    };
+	const removeFromFavorites = (itemId, mediaType) => {
+		setFavorites((prev) =>
+			prev.filter(
+				(item) => item.id !== itemId || item.mediaType !== mediaType
+			)
+		);
+	};
 
-    const isFavorites = (itemId, mediaType) => {
-        return favorites.some(
-            (item)=> item.id === itemId && item.mediaType === mediaType
-        );
-    };
+	const isFavorites = (itemId, mediaType) => {
+		return favorites.some(
+			(item) => item.id === itemId && item.mediaType === mediaType
+		);
+	};
 
+    //提供context給其他元件使用
+	const value = {
+		favorites,
+		addToFavorites,
+		removeFromFavorites,
+		isFavorites,
+	};
 
-    const value = {
-        favorites,
-        addToFavorites,
-        removeFromFavorites,
-        isFavorites,
-    };
-
-    return <MovieContext.Provider value={value}>
-        {children}
-    </MovieContext.Provider>
+	return (
+		<MovieContext.Provider value={value}>{children}</MovieContext.Provider>
+	);
 }
 
 
